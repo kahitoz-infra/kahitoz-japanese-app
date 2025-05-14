8c0a121c5b4b6f65ceffb1a37f70e07d5b3f9c1e
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-05-14 20:34:34 +0530

    [Feature] Updated dashboard with relevant cards topic

--- app/Screens/Dashboard.js
+++ app/Screens/Dashboard.js
@@ -1,51 +1,30 @@
 "use client"
+import Link from 'next/link';
+
 const Dashboard = () => {
-    const onClickN5 = () =>{
-        window.location.href="/Kanji"
-    }
     return (
         <>
             <div className="flex items-center justify-center w-full p-4">
-
-
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full gap-x-2 gap-y-2 ">
-            
-                <div className="flex items-center justify-center">
-                        <div className="bg-red-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl cursor-pointer" onClick={() => window.location.assign("/KanjiList")}>
-                            JLPT Kanji List
 
-                        </div>
-                    </div>
+                    {/* JLPT Kanji List */}
                     <div className="flex items-center justify-center">
-                        <div className="bg-red-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl cursor-pointer" onClick={()=>onClickN5()}>
-                            JLPT N5 Kanji
-
-                        </div>
+                        <Link href="/KanjiList" passHref>
+                            <div className="bg-red-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl cursor-pointer">
+                                Kanji List
+                            </div>
+                        </Link>
                     </div>
+
+                    {/* JLPT N5 Kanji */}
                     <div className="flex items-center justify-center">
-                        <div className="bg-orange-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl">
-                            JLPT N4 Kanji
-
-                        </div>
+                        <Link href="/KanjiCards" passHref>
+                            <div className="bg-red-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl cursor-pointer">
+                                Kanji Cards
+                            </div>
+                        </Link>
                     </div>
-                    <div className="flex items-center justify-center">
-                        <div className="bg-yellow-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl">
-                            JLPT N3 Kanji
 
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <div className="bg-blue-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl">
-                            JLPT N2 Kanji
-
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <div className="bg-green-500 p-4 w-64 h-64 flex items-center justify-center rounded-xl font-bold text-xl">
-                            JLPT N1 Kanji
-
-                        </div>
-                    </div>
                 </div>
             </div>
         </>
