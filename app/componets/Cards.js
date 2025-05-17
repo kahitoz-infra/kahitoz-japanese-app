df846cb912faea83f07762158f4c65e0d681f3e5
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-05-17 21:01:41 +0530

    [Enhancement, Feature] Added the enhancement and api for handling the bookmark correctly

--- app/componets/Cards.js
+++ app/componets/Cards.js
@@ -1,11 +1,9 @@
 "use client";
-import { useState } from "react";
 import { toRomaji } from "wanakana";
 import "./Cards.css";
+import Image from "next/image";
 
-export default function KanjiCard({ kanji, flipAudio }) {
-  const [face, setFace] = useState(true);
-
+export default function KanjiCard({ kanji, flipAudio, face, setFace, onBookmarkToggle }) {
   const handleFlip = () => {
     if (flipAudio) {
       flipAudio.currentTime = 0;
@@ -14,25 +12,41 @@ export default function KanjiCard({ kanji, flipAudio }) {
     setFace(!face);
   };
 
-  return (
-    <div className="flip-card" onClick={handleFlip}>
-      <div className={`flip-card-inner ${face ? "" : "flipped"}`}>
-        {/* Front (Kanji) */}
-        <div className="flip-card-front">
-          <p className="kanji-text">{kanji.kanji}</p>
-        </div>
+  const handleBookmarkClick = (e) => {
+    e.stopPropagation();
+    onBookmarkToggle(kanji.uid, !kanji.marked); // pass new status
+  };
 
-        {/* Back (Details) */}
-        <div className="flip-card-back">
-          <p className="text-2xl text-center">
-            Onyomi: {JSON.parse(kanji.onyomi).join(", ")} ({toRomaji(JSON.parse(kanji.onyomi).join(""))})
-          </p>
-          <p className="text-2xl text-center">
-            Kunyomi: {JSON.parse(kanji.kunyomi).join(", ")} ({toRomaji(JSON.parse(kanji.kunyomi).join(""))})
-          </p>
-          <p className="text-2xl text-center">Meaning: {kanji.english}</p>
+  return (
+      <div className="flip-card" onClick={handleFlip}>
+        <div className={`flip-card-inner ${face ? "" : "flipped"}`}>
+          <div className="flip-card-front">
+            <p className="kanji-text">{kanji.kanji}</p>
+
+            <div
+                className="fixed bottom-2 right-2 z-10 hover:cursor-pointer"
+                onClick={handleBookmarkClick}
+            >
+              {kanji.marked ? (
+                  <Image src="/icons/bookmarked.svg" alt="bookmarked" width={20} height={30} />
+              ) : (
+                  <Image src="/icons/bookmark.svg" alt="bookmark" width={20} height={30} />
+              )}
+            </div>
+          </div>
+
+          <div className="flip-card-back">
+            <p className="text-2xl text-center">
+              Onyomi: {JSON.parse(kanji.onyomi).join(", ")} (
+              {toRomaji(JSON.parse(kanji.onyomi).join(""))})
+            </p>
+            <p className="text-2xl text-center">
+              Kunyomi: {JSON.parse(kanji.kunyomi).join(", ")} (
+              {toRomaji(JSON.parse(kanji.kunyomi).join(""))})
+            </p>
+            <p className="text-2xl text-center">Meaning: {kanji.english}</p>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
