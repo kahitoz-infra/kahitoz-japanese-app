344c440580884936bd5a46a151975eebb8d04144
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-05-22 21:42:39 +0530

    [Feature] Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zenkanji.kahitoz',
+  appName: 'zenkanji',
+  webDir: 'out'
+};
+
+export default config;
