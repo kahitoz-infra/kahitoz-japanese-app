8796663ab4ba730537389b818eb69da81fbe3519
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-06-06 17:08:25 +0530

    Removed unused line

--- app/Screens/Login.js
+++ app/Screens/Login.js
@@ -180,7 +180,6 @@ export default function Login() {
               )}
 
               {/* Main Button */}
-              import Link from 'next/link';
 
               <Link
                 href="/Dashboard"
