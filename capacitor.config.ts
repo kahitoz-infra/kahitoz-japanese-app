85d72cdd9448d6b783f588c34380c3b4045d7eca
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-06-07 21:18:08 +0530

    added the client scope

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.kahitoz.zenkanji',
   appName: 'ZenKanji',
-  webDir: 'out'
+  webDir: 'out',
+  "plugins": {
+    "GoogleAuth": {
+      "scopes": ["profile", "email"],
+      "serverClientId": "430978898167-crns1otvqj1sjkna19el4cm5ful8odiq.apps.googleusercontent.com",
+      "forceCodeForRefreshToken": true
+    }
+  }
 };
 
 export default config;
