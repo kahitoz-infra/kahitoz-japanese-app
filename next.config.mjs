1866c400b7c20aae9a2750102daea35a7dfb5da2
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-06-07 21:18:46 +0530

    updated it for google domain

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         unoptimized: true, // Important for static export if you use <Image />
+        domains: ['lh3.googleusercontent.com'], // Allow Google profile pics
     },
 };
 
