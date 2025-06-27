6e455f6f8bdf3915a4ea3b97d4f85574609a9ac3
naman-1905 <namanoj19@gmail.com>
2025-06-27 16:25:06 +0530

    Added the Learn Now button and navigated to Prequiz

--- app/common_components/CircularButton.js
+++ app/common_components/CircularButton.js
@@ -9,7 +9,7 @@ export default function CircularButton({ href }) {
     <div className="w-full flex justify-end pr-6 mt-6 mb-10">
       <Link href={href} passHref>
         <button
-          className="w-14 h-14 rounded-full flex items-center justify-center bg-[#FFB8C6] dark:bg-[#FF5E2C] text-black dark:text-white text-3xl shadow-md hover:scale-105 transition-transform"
+          className="w-14 h-14 rounded-full flex items-center justify-center bg-[#FFB8C6] dark:bg-[#FF6E41] text-black text-3xl shadow-md hover:scale-105 transition-transform"
           aria-label="Add"
         >
           <Plus size={28} />
