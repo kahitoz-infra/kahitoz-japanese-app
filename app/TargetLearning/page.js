948bb0a3316cc174f7595d1a89254fe1a003f0b4
naman-1905 <namanoj19@gmail.com>
2025-07-29 15:33:34 +0530

    Imported the Adaptive Quiz component in Target Learning page.js

--- app/TargetLearning/page.js
+++ app/TargetLearning/page.js
@@ -1,9 +1,12 @@
-import React from 'react'
+// app/Quiz/page.js
 
-function TargetLearningQuiz() {
+import { Suspense } from 'react';
+import AdaptiveQuizPageContent from './AdaptiveQuiz';
+
+export default function AdaptiveQuiz() {
   return (
-    <div>Target Learning Page</div>
-  )
+    <Suspense fallback={<p className="p-4 text-center">Loading quiz...</p>}>
+      <AdaptiveQuizPageContent/>
+    </Suspense>
+  );
 }
-
-export default TargetLearningQuiz
