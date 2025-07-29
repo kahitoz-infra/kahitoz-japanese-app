9844ae26c5ff42e003b1a378752c02da6cdc2878
naman-1905 <namanoj19@gmail.com>
2025-07-29 16:14:05 +0530

    Imported the Quiz Summary Card in Adaptive Quiz Summary Page

---
+++ app/AdaptiveQuizSummary/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuizSummaryCard from '../common_components/QuizSummaryCard'
+
+function page() {
+  return (
+    <div>
+        <QuizSummaryCard/>
+    </div>
+  )
+}
+
+export default page
