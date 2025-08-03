fa4a5b2f82a0dba6ab9f4c9dc216e5522deab0d2
naman-1905 <namanoj19@gmail.com>
2025-08-03 22:51:24 +0530

    Added a Adaptive Sets page and imported "Adaptive Quiz Sets" component which stores the quiz locally

---
+++ app/(Sets)/AdaptiveSets/page.js
@@ -0,0 +1,12 @@
+import AdaptiveQuizSets from "@/app/common_components/AdaptiveQuizSets";
+
+
+function AdaptiveSets() {
+  return (
+    <div>
+        <AdaptiveQuizSets/>
+    </div>
+  )
+}
+
+export default AdaptiveSets
