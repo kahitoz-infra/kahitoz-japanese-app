e0252d90f52f0f1c17f31bf24e9847caa988d866
naman-1905 <namanoj19@gmail.com>
2025-08-04 15:05:37 +0530

    Added the Cherry Blossom Effect to the Adaptive Sets page

--- app/(Sets)/AdaptiveSets/page.js
+++ app/(Sets)/AdaptiveSets/page.js
@@ -1,10 +1,14 @@
 import AdaptiveQuizSets from "@/app/common_components/AdaptiveQuizSets";
+import CherryBlossomSnowfall from "@/app/common_components/CherryBlossomSnowfall";
 
 
 function AdaptiveSets() {
   return (
     <div>
+      <CherryBlossomSnowfall/>
+      <div>
         <AdaptiveQuizSets/>
+        </div>
     </div>
   )
 }
