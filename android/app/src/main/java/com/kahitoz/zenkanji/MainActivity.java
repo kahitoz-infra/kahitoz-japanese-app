c241fed5fb847cab6d8a74ea534f2b3e39ca2017
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-08-25 20:47:28 +0530

    Updated the required configuration

---
+++ android/app/src/main/java/com/kahitoz/zenkanji/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kahitoz.zenkanji;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,17 +4,13 @@ const config: CapacitorConfig = {
   appId: 'com.kahitoz.zenkanji',
   appName: 'ZenKanji',
   webDir: 'out',
-  plugins: {
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      serverClientId: '430978898167-crns1otvqj1sjkna19el4cm5ful8odiq.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
-    },
-  },
-  android: {
-
-    path: '../kahitoz-japanese-android-app/android',
-  },
+  "plugins": {
+    "GoogleAuth": {
+      "scopes": ["profile", "email"],
+      "serverClientId": "430978898167-crns1otvqj1sjkna19el4cm5ful8odiq.apps.googleusercontent.com",
+      "forceCodeForRefreshToken": true
+    }
+  }
 };
 
 export default config;
