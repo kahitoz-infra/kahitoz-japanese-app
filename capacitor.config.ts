aafa96f5420613710def56a46fbf86a28afe264e
Kahitoz <kshitiz.sinha.025@gmail.com>
2025-08-25 12:34:28 +0000

    Changes in the capacitor config js

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,13 +4,17 @@ const config: CapacitorConfig = {
   appId: 'com.kahitoz.zenkanji',
   appName: 'ZenKanji',
   webDir: 'out',
-  "plugins": {
-    "GoogleAuth": {
-      "scopes": ["profile", "email"],
-      "serverClientId": "430978898167-crns1otvqj1sjkna19el4cm5ful8odiq.apps.googleusercontent.com",
-      "forceCodeForRefreshToken": true
-    }
-  }
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '430978898167-crns1otvqj1sjkna19el4cm5ful8odiq.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
+  android: {
+
+    path: '../kahitoz-japanese-android-app/android',
+  },
 };
 
 export default config;
