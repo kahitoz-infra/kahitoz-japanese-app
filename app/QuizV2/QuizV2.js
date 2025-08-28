81b1a4a2ad5feae3c3cae79ee46b35fed09caded
naman-1905 <namanoj19@gmail.com>
2025-08-28 23:21:13 +0530

    Created Quiz v2 page

---
+++ app/QuizV2/QuizV2.js
@@ -0,0 +1,13 @@
+import FabMenu from "./components/CircularModal";
+
+import React from 'react'
+
+function QuizV2() {
+  return (
+    <div>
+      <FabMenu/>
+    </div>
+  )
+}
+
+export default QuizV2
